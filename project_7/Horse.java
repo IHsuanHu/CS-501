6a26a6da76239000d6d64d734c3cb57d1c9dc240
IHsuanHu <ihsuanhu@gmail.com>
2023-04-10 20:51:28 -0400

    final

---
+++ project_7/Horse.java
@@ -0,0 +1,27 @@
+package project_7;
+
+//I pledge my honor that I have abided by the Stevens Honor System.
+//I-Hsuan Hu
+public class Horse{
+    private String name;
+    private String color;
+    private int birthYear;
+    public String getName(){
+        return name;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getBirthYear(){
+        return birthYear;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public void setColor(String c){
+        color = c;
+    }
+    public void setBirthYear(int y){
+        birthYear = y;
+    }
+}
---
+++ project_7/RaceHorse.java
@@ -0,0 +1,13 @@
+package project_7;
+
+//I pledge my honor that I have abided by the Stevens Honor System.
+//I-Hsuan Hu
+public class RaceHorse extends Horse{
+    private int races;
+    public int getRaces(){
+        return races;
+    }
+    public void setRaces(int r){
+        races = r;
+    }
+}
